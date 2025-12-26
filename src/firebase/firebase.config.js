a2317f123aa997aa632065e77f6923265c94cdb6
Pritom099 <pritompaul515@gmail.com>
2025-12-26 21:25:55 +0600

    env commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhHmrErUv8fb6BcH0lAii2OpltM14sayU",
-  authDomain: "fir-fighter-cc73f.firebaseapp.com",
-  projectId: "fir-fighter-cc73f",
-  storageBucket: "fir-fighter-cc73f.firebasestorage.app",
-  messagingSenderId: "1037286300968",
-  appId: "1:1037286300968:web:5dc5f3ac6bcecacf7bc41d"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_ppId
 };
 
 // Initialize Firebase
